195440e6fd20c2a581a9b88d420fa4741437e6a7
Alexander Dean <alex@snowplowanalytics.com>
2013-06-07 20:45:20 +0200

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,6 @@ import sbt._
 
 object Dependencies {
   val resolutionRepos = Seq(
-    ScalaToolsSnapshots,
     "Akka Repository" at "http://repo.akka.io/releases/",
     "Spray Repository" at "http://repo.spray.cc/"
   )
