4ca32d36082e9d175c750a01ec13e81bb04842f9
Alex Dean <alexander.dean@keplarllp.com>
2013-06-15 19:46:40 +0100

    Some more fixes

--- scripts/install-spark-run-job.sh
+++ scripts/install-spark-run-job.sh
@@ -11,7 +11,7 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 #
-# Version:     0.1.2
+# Version:     0.1.3
 # URL:         https://github.com/snowplow/spark-example-project/blob/master/scripts/install-spark-run-job.sh
 #
 # Authors:     Alex Dean
@@ -19,11 +19,9 @@
 # License:     Apache License Version 2.0
 
 # Configure variables
-HADOOP_HOME=/home/hadoop/
+HADOOP_HOME=/home/hadoop
 SCALA_HOME=$HADOOP_HOME/scala-2.9.3
 SPARK_HOME=$HADOOP_HOME/spark-0.7.2
-SPARK_ENV=$SPARK_HOME/conf/spark-env.sh
-SPARK_JOBS=$SPARK_HOME/jobs
 MASTER_HOST=$(grep -i "job.tracker<" /home/hadoop/conf/mapred-site.xml | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 MASTER_PORT=7077
 MASTER=spark://$MASTER_HOST:$MASTER_PORT
@@ -37,8 +35,9 @@ tar -xvzf scala-2.9.3.tgz
 tar -xvzf download-spark-0.7.2-prebuilt-hadoop1
 
 # Build Spark environment
-touch $SPARK_ENV
-cat >$SPARK_ENV <<EOL
+spark_env=$SPARK_HOME/conf/spark-env.sh
+touch $spark_env
+cat >$spark_env <<EOL
 export SPARK_MASTER_IP=$MASTER
 export SCALA_HOME=$SCALA_HOME
 export SPARK_HOME=$SPARK_HOME
@@ -48,25 +47,26 @@ export SPARK_JAVA_OPTS=\"-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps 
 EOL
 
 # Install config file and jars
-cp /home/hadoop/conf/core-site.xml /home/hadoop/spark/conf/
-cp /home/hadoop/hadoop-core.jar /home/hadoop/spark/lib_managed/jars/hadoop-core-1.0.3.jar 
-cp /home/hadoop/lib/gson-* /home/hadoop/spark/lib_managed/jars/
-cp /home/hadoop/lib/aws-java-sdk-* /home/hadoop/spark/lib_managed/jars/
-cp /home/hadoop/lib/emr-metrics* /home/hadoop/spark/lib_managed/jars/
+cp $HADOOP_HOME/conf/core-site.xml $SPARK_HOME/conf/
+cp $HADOOP_HOME/hadoop-core.jar $SPARK_HOME/lib_managed/jars/hadoop-core-1.0.3.jar 
+cp $HADOOP_HOME/lib/gson-* $SPARK_HOME/lib_managed/jars/
+cp $HADOOP_HOME/lib/aws-java-sdk-* $SPARK_HOME/lib_managed/jars/
+cp $HADOOP_HOME/lib/emr-metrics* $SPARK_HOME/lib_managed/jars/
 
 # Start Spark master or Spark worker daemon
 grep -Fq '"isMaster":true' /mnt/var/lib/info/instance.json
 if [ $? -eq 0 ];
 then
         # Start master
-        /home/hadoop/spark/bin/start-master.sh
+        $SPARK_HOME/bin/start-master.sh
         
         # Run our job (if we have one)
-        if [ ${#} -ge 1 ]; then
-                jar_uri=${1}
-                cd $SPARK_JOBS
+        if [ $# -ge 1 ]; then
+                jar_uri=$1
+                jobs=$SPARK_HOME/jobs
+                cd $jobs
                 wget $jar_uri
-                job_path=$SPARK_JOBS/$(basename $jar_uri)
+                job_path=$jobs/$(basename $jar_uri)
                 java -jar $job_path ${*:2}
         fi
 
@@ -78,5 +78,5 @@ else
                 nc -z  $MASTER_HOST $MASTER_PORT
         done
         echo "Connecting to the master was successful"
-        /home/hadoop/spark/bin/spark-daemon.sh start spark.deploy.worker.Worker $MASTER
+        $SPARK_HOME/bin/spark-daemon.sh start spark.deploy.worker.Worker $MASTER
 fi
